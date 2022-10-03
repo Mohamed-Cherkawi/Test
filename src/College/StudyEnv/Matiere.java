99e99bf6d6ab0f8b54e048c4757f3580f99dd141
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2022-10-03 18:12:30 +0100

    Added A HashMap in the constructor that adds all the subjects available and provided some methods to loop through the subjects array hashmap and to get data by its key

--- src/College/StudyEnv/Matiere.java
+++ src/College/StudyEnv/Matiere.java
@@ -1,8 +1,51 @@
 package College.StudyEnv;
 
+import java.util.HashMap;
+
 public class Matiere {
 
+    private HashMap<Integer, String> matiereArray = new HashMap<>();
     private byte id ;
     private String nom ;
-    // Salle Class
+
+// Salle Class
+    public Matiere() {
+        matiereArray.put(1, "Physique");
+        matiereArray.put(2, "Science Vie & Terre");
+        matiereArray.put(3, "Programmation");
+        matiereArray.put(4 , "Sport");
+        matiereArray.put(5 , "Philosophie");
+        matiereArray.put(6 , "Mathematique");
+    }
+    public void setMatiereArra(HashMap<Integer, String> matiereArra) {
+        this.matiereArray = matiereArra;
+    }
+
+    public int loopMatiereArr_getNumOfKeys(){
+        int counter = 0;
+        for (int key : matiereArray.keySet()) {
+            System.out.println("id_matiere : { " + key + " } -->> : [ " + matiereArray.get(key) + " ]");
+            counter++;
+        }
+        return counter ;
+    }
+
+    public String getMatiereNameById(byte key){
+        return matiereArray.get(key);
+    }
+    public byte getId() {
+        return id;
+    }
+
+    public void setId(byte id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 }
