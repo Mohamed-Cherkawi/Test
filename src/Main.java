ac01374f52a44b9e7d8c2c77be566783dae2704b
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2022-10-03 18:56:00 +0100

    Create the required code to get things stand out , also create some static methods that helps in ( Requesting User input data - Validation ...) , to avoid repeating code and ensure code reusibility

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import College.* ;
+import College.StudyEnv.Matiere;
 import College.template.Role;
+import College.users.Admin;
+import College.users.Enseignant;
 
 import java.util.Scanner;
 import java.util.HashMap; // import the HashMap class
@@ -9,42 +12,83 @@ public class Main {
 
     public static void main(String[] args) {
         // Defining Variables :
-        short choosenNum = 0 ;
-        int id , roleId ;
-        String nom , prenom , telephone , email ;
-        Scanner scanerObj = new Scanner(System.in); // Create an object from Scanner Class to read inputs
+        int id , roleId , subjectId , departementId, numOfKeys , choosenNum = 0 ;
+        String roleName , collegeName , subjectName , departementName , collegeSite , nom , prenom , telephone , email , date ;
+        HashMap<Integer, String> rolesArray , subjectsArray ;
+      //  Scanner scanerObj = new Scanner(System.in); // Create an object from Scanner Class to read inputs
         // Application Menu
-        System.out.print("############################### Welcome At Win Academy App ############################### \n" +
-                " Veuillez Entrez Le Nom du College : ");
+        System.out.print("############################### Bienvenue à Win Academy App ############################### \n" +
+                " Veuillez Entrez Le Nom Du College : ");
         // Stores the  college name
-        String collegeName = scanerObj.nextLine();
-        System.out.println("Veuillez nous donnez des informations sur vous : ");
-        System.out.println("|->> Veuillez Entrez un id : ");
+        collegeName = stringScannerMeth((byte) 0);
+        System.out.print(" Veuillez Entrez Le Site Internet De Votre College : ");
+        collegeSite = stringScannerMeth((byte) 2);
+        //  Creating a new College Object
+        College collegeObj = new College((byte) 1 , collegeName , collegeSite);
+        // Getting some informations about the user
+        System.out.println(" Veuillez Nous Donnez Des Informations Sur Vous : ");
+        System.out.println("|->> Veuillez Entrez Un Id : ");
         id = validateUserInputForNum();
-        System.out.println(id);
-        System.out.println("|->> Veuillez Entrez votre nom : ");
+        System.out.println("|->> Veuillez Entrez Votre Nom : ");
         nom = stringScannerMeth((byte) 0);
-        System.out.println(nom);
-        System.out.println("|->> Veuillez Entrez votre prenom : ");
+        System.out.println("|->> Veuillez Entrez Votre Prenom : ");
         prenom = stringScannerMeth((byte) 0);
-        System.out.println(prenom);
-        System.out.println("|->> Veuillez Entrez votre telephone : ");
+        System.out.println("|->> Veuillez Entrez Votre Telephone : ");
         telephone = stringScannerMeth((byte) 9);
-        System.out.println(telephone);
-        System.out.println("|->> Veuillez Entrez votre email : ");
+        System.out.println("|->> Veuillez Entrez Votre Email : ");
         email = stringScannerMeth((byte) 2);
-        System.out.println(email);
-        System.out.println("|->> Veuillez Entrez votre role dans le college : ");
-        // We want to instanciate the Role Class to get all the roles stored in a HashMap
-        Role RoleClass = new Role();
-        roleId = getChoosenUserInputNumber((byte) 1 , (byte) 3 , "l' id du role");
-        System.out.println(roleId);
+        // Instanciate the Role Object
+        Role roleObj = new Role();
+        // Displaying the roles and getting the number of keys to set the limit
+        numOfKeys = roleObj.loopRoleArr_getNumOfKeys();
+        roleId = getChoosenUserInputNumber( 1 , numOfKeys , "l' id du role");
+        roleName = roleObj.getRoleNameById(roleId);
 
+        // Creating new user ( Admin || Teacher || Student ) Depends on the role selected
+        if( roleId == 1 ){
+            // Create the Admin Obj
+            Admin adminObj = new Admin( (short) id , nom , prenom , telephone , email);
+            System.out.println("\n \n *******************************************\n Admin A été Bien Créer \n *******************************************\n \n");
+            System.out.println(adminObj);
+        }else if( roleId == 2){
+            // This function asks the user the join date ( Day & month & year ) and returns the date as result formatted like ( dd - mm - yyyy )
+            date = getDate();
+            // Creating the Subject Object to create All the Subjects available
+            Matiere matiereObj = new Matiere();
+            // Displaying all the subjects and getting the number of keys to set the limit
+            numOfKeys = matiereObj.loopMatiereArr_getNumOfKeys();
+            // Asking the teacher to type the id of the subject
+            subjectId = getChoosenUserInputNumber(1 , numOfKeys ,"L'id De Matiere Que Vous Enseignez");
+            // Getting the name of subject by the given id
+            subjectName = matiereObj.getMatiereNameById((byte) subjectId);
+            // Creating the Departement Object to create All the Departements available
+            Departement departementObj = new Departement();
+            // Displaying all the departement and getting the number of keys to set the limit
+            numOfKeys = departementObj.loopDepartsArr_getNumOfKeys();
+            // Asking the teacher to type the id of the departement that he teaches in
+            departementId = getChoosenUserInputNumber(1 , numOfKeys ,"L'id De Département Dont Vous Enseignez");
+            // Getting the name of departement by the given id
+            departementName = departementObj.getDepartementNameById((byte) departementId);
+            // Creating the teacher Object
+            Enseignant teacherObj = new Enseignant((short) id , nom , prenom , telephone ,email ,date , subjectName , departementName);
+            System.out.println("\n \n ******************************************* \n Enseignant A été Bien Créer \n ******************************************* \n \n");
+            System.out.println(teacherObj);
+        }
         do {
-            System.out.println("############################### Welcome At ( " + collegeName + " ) College ############################### \n" +
-                    "1 ->> Gerer un département \n 2 ->> Gerer un Enseignant \n 3 ->> Gerer un étudiant \n 4 ->> Affichage \n 0 ->> Quittez l'application \n");
-
-            choosenNum = getChoosenUserInputNumber( (byte) 0 , (byte) 4 , "le numero");
+            System.out.println("############################### Bienveue A ( " + collegeObj.getNom() + " ) College ############################### \n" +
+                               "############################### Vous etes un/une { " + roleName +" } ##############################");
+            if( roleId == 1 ) {
+                // Create a new Admin
+                System.out.println(" 1 ->> Gerer un département \n 2 ->> Gerer un Enseignant \n 3 ->> Gerer un étudiant \n 4 ->> Affichage \n 0 ->> Quittez l'application");
+                choosenNum = getChoosenUserInputNumber( 0 , 4 , "le numero");
+            }else if( roleId == 2 ) {
+                System.out.println(" 1 ->> Gerer un Enseignant \n 2 ->> Gerer un étudiant \n 3 ->> Affichage \n 0 ->> Quittez l'application");
+                choosenNum = getChoosenUserInputNumber( 0 , 3 , "le numero");
+            }else {
+                System.out.println(" 1 ->> Affichage \n 0 ->> Quittez l'application");
+                choosenNum = getChoosenUserInputNumber( 0 , 1 , "le numero");
+            }
+            // Getting the chosen number
             switch (choosenNum) {
                 case 1:
                     manageDepartement();
@@ -60,17 +104,17 @@ public class Main {
             }
 
         }while (choosenNum != 0) ;
-         System.out.println("##################### See you later  At Win Academy App ;) ###########################");
+         System.out.println("##################### A Plus Tard Win Academy App ;) ###########################");
     }
 
     public static void manageDepartement(){
         // Variables
-        short choosenNum = 0 ;
+        int choosenNum = 0 ;
         do {
             System.out.println("============================================== Departement Section ==============================================================\n 1 ->> Ajouter un département \n" +
-                    " 2 ->> Modifier un département \n 3 ->> Afficher un ou plusieurs départements \n 4 ->> Supprimer un département \n 0 ->> Retour \n");
+                    " 2 ->> Modifier Un Département \n 3 ->> Afficher Un Ou Plusieurs Départements \n 4 ->> Supprimer Un Département \n 0 ->> Retour \n");
 
-            choosenNum = getChoosenUserInputNumber( (byte) 0 , (byte) 4 , "le numero");
+            choosenNum = getChoosenUserInputNumber( 0 ,  4 , "Le Numero");
 
             switch (choosenNum) {
                 case 1:
@@ -97,19 +141,19 @@ public class Main {
 
     public static void show(){}
 
-    public static short getChoosenUserInputNumber(short start , short end , String text){
+    public static int getChoosenUserInputNumber(int start , int end , String text){
         // Defining Variables :
-        short choosenNum ;
+        int choosenNum ;
         Scanner scanerObj = new Scanner(System.in); // Create an object from Scanner Class to read inputs
-        System.out.print(" Veuillez Entrez " + text + " içi : ");
+        System.out.print(" \nVeuillez Entrez " + text + " Içi : ");
         do {
-            if (scanerObj.hasNextByte()) { // Checking what users typed before he submittes , to know what type of data he entered
-                choosenNum = scanerObj.nextByte();
+            if (scanerObj.hasNextInt()) { // Checking what users typed before he submittes , to know what type of data he entered
+                choosenNum = scanerObj.nextInt();
                 if (choosenNum >= start && choosenNum <= end) {
                     break;
                 }
             }
-            System.out.print(" Veuillez entrez un numero existant : ");
+            System.out.print(" Veuillez Entrez Un Numero Existant : ");
             // Clearing Buffer so it refreshes , to read again from the user
             scanerObj.next();
 
@@ -127,7 +171,7 @@ public class Main {
                 choosenNumber = scanerObj.nextInt();
                 break;
             }
-            System.out.print(" Veuillez entrez un numero  : ");
+            System.out.print(" Veuillez Entrez Un Numero  : ");
             // Clearing Buffer so it refreshes , to read again from the user
             scanerObj.next();
 
@@ -144,12 +188,41 @@ public class Main {
             if( Text.length() > length ){
                 break;
             }
-            System.out.print(" Champ Textuelle ne peut pas être vide ou inférieure à [ "+ length + 1 +" ] Caractère : ");
+            System.out.print(" Champ Textuelle Ne Peut Pas être Vide Ou Inférieure à [ "+ ( length + 1 ) +" ] Caractère : ");
 
         }while(true);
 
         return Text ;
 
+    }
+
+    public static int numberScannerMeth(int length){
+        int number ;
+        Scanner scanerObj = new Scanner(System.in); // Create an object from Scanner Class to read inputs
+        do {
+            number = scanerObj.nextInt();
+            if( String.valueOf(number).length() == length ){
+                break;
+            }
+            System.out.print(" Champ Numérique Ne Peut Pas être Vide Et Doit être égale à [ "+ length +" ] Nombres : ");
+
+        }while(true);
+
+        return number ;
+    }
+
+    public static String getDate(){
+        int day , month , year ;
+        String date ;
+        System.out.println(" |-> Veuillez Entrez Votre Date D' entrée  ");
+        System.out.println("Entrez Le Jour En ( dd ) : ");
+        day = numberScannerMeth(2);
+        System.out.println("Entrez Le Mois En ( mm ) : ");
+        month = numberScannerMeth(2);
+        System.out.println("Entrez L' année En ( yyyy ) : ");
+        year = numberScannerMeth(4);
+        date = day + " - " + month + " - " + year ;
+        return date ;
     }
 
 }
