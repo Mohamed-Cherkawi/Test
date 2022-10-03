aaa1bead7f015fedd8feb3ec5eb0d570993572ba
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2022-10-03 18:52:21 +0100

    put values to A HashMap in the constructor that adds all the roles available and provided some methods to loop through the roles array hashmap and to get data by its key

--- src/College/template/Role.java
+++ src/College/template/Role.java
@@ -2,26 +2,27 @@ package College.template;
 import java.util.HashMap; // import the HashMap class
 
 public class Role {
-    private HashMap<Integer, String>  RoleArra;
+    private HashMap<Integer, String> RoleArray = new HashMap<>();
     private byte id;
     private String nom ;
 
-    public void setRoleArra(HashMap<Integer, String> roleArra) {
-        RoleArra = roleArra;
-    }
-
-    public HashMap<Integer, String> getRoleArra() {
-        return RoleArra;
-    }
-
     public Role() {
-        HashMap<Integer, String> RoleArray = new HashMap<Integer, String>();
-        RoleArray.put(1, "Admin");
-        RoleArray.put(2, "Professeur");
-        RoleArray.put(3, "Etudiant");
-        // Setting The role array
-        this.setRoleArra(RoleArray);
-        System.out.println(this.getRoleArra());
+        this.RoleArray.put(1, "Admin");
+        this.RoleArray.put(2, "Professeur");
+        this.RoleArray.put(3, "Etudiant");
     }
 
+    public int loopRoleArr_getNumOfKeys(){
+        // Looping through the Hash
+        int counter = 0 ;
+        for (int key : RoleArray.keySet()) {
+            System.out.println("id_role : { " + key + " } -->> : [ " + RoleArray.get(key) + " ]");
+            counter++;
+        }
+        return counter ;
+    }
+
+    public String getRoleNameById(int key){
+        return RoleArray.get(key);
+    }
 }
