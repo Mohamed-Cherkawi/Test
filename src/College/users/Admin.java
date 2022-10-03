27f5a3689c6d7fb7b940a84200c72604c8fa5545
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2022-10-03 18:46:32 +0100

    Provided a constructor to give the value for each property in the class by the user and overided the toString method to print out the object proprely

--- src/College/users/Admin.java
+++ src/College/users/Admin.java
@@ -4,14 +4,14 @@ import College.template.User;
 
 public class Admin  extends User {
 
-    public Admin(short id , String nom , String prenom , String telephone , String email) {
-    id = id ;
-    nom = nom ;
-    prenom = prenom ;
-    telephone = telephone ;
-    email = email ;
+    public Admin(short id, String nom, String prenom, String telephone, String email) {
+        super(id, nom, prenom, telephone, email);
     }
-
+    @Override
+    public String toString(){
+        return "{ \n   Id -> " + getId() + "\n   Nom -> " + getNom() + "\n   Prenom -> " + getPrenom() + "\n   Telephone -> " + getTelephone()
+                + "\n   Email -> " + getEmail() + " \n}";
+    }
     @Override
     public void insert() {
 
--- src/College/users/Enseignant.java
+++ src/College/users/Enseignant.java
@@ -1,18 +1,46 @@
 package College.users;
 
+import College.Departement;
+import College.StudyEnv.Matiere;
 import College.template.User;
 
 public class Enseignant extends User {
 
     // User Class
     private String dateFonction;
-    //Matiere
+    private String matiere;
+    private String departement;
     //Departement
+
+
+    public Enseignant(short id, String nom, String prenom, String telephone, String email, String dateFonction, String matiere, String departement) {
+        super(id, nom, prenom, telephone, email);
+        this.dateFonction = dateFonction;
+        this.matiere = matiere;
+        this.departement = departement;
+    }
 
     public String afficherFicheEnseignant(){
         return null ;
     }
 
+    @Override
+    public String toString(){
+        return "{ \n   Id -> " + getId() + "\n   Nom -> " + getNom() + "\n   Prenom -> " + getPrenom() + "\n   Telephone -> " + getTelephone()
+                + "\n   Email -> " + getEmail() + "\n   Date De Fonction -> " + getDateFonction() +  "\n   Enseigne -> " + getMatiere()
+                + "\n   En Département de -> " + getDepartement() + " \n}";
+    }
+    public String getDateFonction() {
+        return dateFonction;
+    }
+
+    public String getMatiere() {
+        return matiere;
+    }
+
+    public String getDepartement() {
+        return departement;
+    }
     @Override
     public void insert() {
 
