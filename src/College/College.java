4805a65fc091fbbbd976061427cb59b102d9ddb4
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2022-10-03 18:52:56 +0100

    Code refactoring

--- src/College/College.java
+++ src/College/College.java
@@ -3,10 +3,17 @@ package College;
 public class College {
 
     private byte id ;
+
     private String nom ;
     private String Lien_Site_Internet ;
 
-    public College(String nom) {
+    public College(byte id, String nom, String lien_Site_Internet) {
+        this.id = id;
         this.nom = nom;
+        this.Lien_Site_Internet = lien_Site_Internet;
+    }
+
+    public String getNom() {
+        return nom;
     }
 }
