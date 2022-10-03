af3700dc62bdf15419d6b77df91d38b581ce2d8b
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2022-10-03 18:11:32 +0100

    Added A HashMap in the constructor that adds all the departements available and provided some methods to loop through the departements array hashmap and to get data by its key

--- src/College/Departement.java
+++ src/College/Departement.java
@@ -1,9 +1,41 @@
 package College;
 
+import java.util.HashMap;
+
 public class Departement {
 
+    private HashMap<Integer, String> DepartementArray = new HashMap<>();
     private byte id ;
     private String nom ;
     // College Class
     // Responsable : Enseignant
+
+    public Departement() {
+        DepartementArray.put(1 , "Sciences");
+        DepartementArray.put(2 , "Expériences");
+        DepartementArray.put(3 , "Sport");
+        DepartementArray.put(4 , "Litérature");
+        DepartementArray.put(5 , "Languages");
+    }
+
+    public int loopDepartsArr_getNumOfKeys(){
+        // Looping through the Hash
+        int counter = 0 ;
+        for (int key : DepartementArray.keySet()) {
+            System.out.println("id_departement : { " + key + " } -->> : [ " + DepartementArray.get(key) + " ]");
+            counter++;
+        }
+        return counter ;
+    }
+
+    public String getDepartementNameById(int key){
+        return DepartementArray.get(key);
+    }
+    public void addDepartement(){}
+
+    public void showDepartements(){}
+
+    public void showDepartementById(){}
+
+    public void deleteDepartementById(){}
 }
