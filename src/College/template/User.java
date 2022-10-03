009d040c6c9b796454f1fbac705be65babdefb78
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2022-10-03 18:49:45 +0100

    Create Constructor for the abstract user class so other subclasses can inizialise these properties in their constructor & Create setters and getters for its properties

--- src/College/template/User.java
+++ src/College/template/User.java
@@ -8,10 +8,57 @@ public abstract class User {
     private String prenom ;
     private String telephone ;
     private String email ;
-    private String password ;
     // private  role ;
 
-    //Methods
+    // Abstract Methods
+    public User(short id, String nom, String prenom, String telephone, String email) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.telephone = telephone;
+        this.email = email;
+    }
+
+    public short getId() {
+        return id;
+    }
+
+    public void setId(short id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public abstract void insert();
     public abstract void update();
     public abstract String search();
